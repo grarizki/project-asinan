589128f22b0aa265cf189a600417aa3ef21de73f
grarizki <28901232+grarizki@users.noreply.github.com>
2021-11-02 12:41:22 +0700

    prepare navbar

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="container mx-auto box-border h-32 w-32 p-4 border-4 ...">
-      <h1 className="text-purple-700">TULISAN INI DIDALEM BOX</h1>
+    <div className="flex mx-auto bg-gradient-to-b from-gray-800 to-gray-700">
+      <h1 className="text-white">Kliwon Night</h1>
     </div>
   );
 }
