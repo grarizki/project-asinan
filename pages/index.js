304b706d15faa371fc3bc36555420cb9050a9637
grarizki <28901232+grarizki@users.noreply.github.com>
2021-11-03 10:15:07 +0700

    position style navbar

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex mx-auto p-1 bg-gradient-to-b from-gray-800 to-gray-700">
       <h1 className="text-white ml-4">Kliwon Night</h1>
-      <div className="flex right-0 top-0">
+      <div className="flex absolute right-8">
         <div className="flex ml-6">
           <h1 className="text-white">Home</h1>
         </div>
