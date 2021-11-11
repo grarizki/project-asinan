f5aa972076125ee80782f31b2d41c8480e8d296d
grarizki <28901232+grarizki@users.noreply.github.com>
2021-11-11 13:32:27 +0700

    change favicon, route link

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,36 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <div className="flex mx-auto p-1 bg-gradient-to-b from-gray-800 to-gray-700">
-      <h1 className="text-white ml-4">Kliwon Night</h1>
-      <div className="flex absolute right-8">
-        <div className="flex ml-6">
-          <h1 className="text-white">Home</h1>
+    <div className="header">
+      <div className="flex mx-auto p-1 bg-gradient-to-b from-gray-800 to-gray-700">
+        <h1 className="text-white ml-4">Kliwon Night</h1>
+        <div className="flex absolute right-8">
+          <div className="flex ml-6">
+            <Link href="/">
+              <a className="text-white">Home</a>
+            </Link>
+          </div>
+          <div className="flex ml-4">
+            <Link href="/blog">
+              <a className="text-white">Blog</a>
+            </Link>
+          </div>
+
+          <div className="flex ml-4">
+            <Link href="shop">
+              <a className="text-white">Shop</a>
+            </Link>
+          </div>
+          <div className="flex ml-4">
+            <Link href="/event">
+              <a className="text-white">Event</a>
+            </Link>
+          </div>
         </div>
-        <div className="flex ml-4">
-          <h1 className="text-white">Blog</h1>
-        </div>
-        <div className="flex ml-4">
-          <h1 className="text-white">Shop</h1>
-        </div>
-        <div className="flex ml-4">
-          <h1 className="text-white">Event</h1>
-        </div>
+      </div>
+      <div className="hero">
+        <div className="bg-gray-300"></div>
       </div>
     </div>
   );
