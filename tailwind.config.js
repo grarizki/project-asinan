007972615b9ec408037ef0058ebe1d464d1c9fb0
grarizki <28901232+grarizki@users.noreply.github.com>
2021-11-15 10:41:34 +0700

    install google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Source Sans Pro', 'Arial', 'sans-serif'],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
