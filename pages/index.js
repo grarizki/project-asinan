c984d73637ed7508bdf300bc9351196647c10b42
grarizki <28901232+grarizki@users.noreply.github.com>
2021-11-17 14:19:27 +0700

    layout fix

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,28 @@
 import Image from 'next/image';
 import logo from '../assets/Logo.png';
 
+import { useState } from 'react';
+
 export default function Home() {
+  const [dropdown, setDropdown] = useState(false);
   return (
     <div className="bg-gradient-to-t from-black-dark to-black-light min-h-screen text-white">
-      <nav className="py-3 px-3">
+      <nav className="py-5 px-8">
         <div className="container mx-auto">
           <div className="flex items-center">
             <a href="#" className="w-6/12 flex items-center">
               <div className="w-10 h-10 bg-gradient-to-t from-red-800 to-red-600 rounded flex items-center justify-center mr-4 shadow-2xl">
                 KN
               </div>
-              Kliwon Night
+              Killone Night
             </a>
-            <div className="w-8/12 items-center justify-center">
+            <div className="w-9/12 items-center justify-center">
               <input
                 className="text bg-trueGray-500 py-1 px-3 rounded-full bg-search"
                 placeholder="Search"
               />
             </div>
-            <div className="w-2/12">
+            <div className="w-4/12">
               <ul className="flex space-x-5">
                 <li>
                   <a href="#" className="hover:underline">
@@ -36,10 +39,14 @@ export default function Home() {
                     Shop
                   </a>
                 </li>
-                <li>
+                <li className="relative">
                   <a href="#" className="hover:underline">
                     Event
                   </a>
+                  <ul className="absolute">
+                    <li>International</li>
+                    <li>Indonesia</li>
+                  </ul>
                 </li>
               </ul>
             </div>
